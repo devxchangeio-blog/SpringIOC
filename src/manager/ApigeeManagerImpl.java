c050dcf01211b97820aaf0a3ed65c3e65416f603
karthy86 <karthy86@live.com>
2014-10-01 03:02:53 -0400

    Spring IOC
    Spring IOC

---
+++ src/manager/ApigeeManagerImpl.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package manager;
+
+/**
+ * @author Karthy
+ *
+ */
+public class ApigeeManagerImpl implements  ApiManager{
+
+	public String popQuestion() {
+		
+		return "Are you using Apigee API Management ? ";
+	}
+
+}
---
+++ src/manager/MasheryManagerImpl.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package manager;
+
+/**
+ * @author Karthy
+ * 
+ */
+public class MasheryManagerImpl implements ApiManager {
+
+	public String popQuestion() {
+
+		return "Are you using Mashery API Management ? ";
+	}
+
+}
